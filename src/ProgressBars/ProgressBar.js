e90ce7bb0a2695f7242490c493e3d7224b08dd79
Bryan Yee <yee.bryan.c@gmail.com>
2021-07-10 12:13:34 -0700

    Allow progress bars to wait for the one before it

--- src/ProgressBars/ProgressBar.js
+++ src/ProgressBars/ProgressBar.js
@@ -1,20 +1,85 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef } from 'react';
+import { Component, createRef, useEffect, useRef } from 'react';
 
 import styles from './ProgressBar.module.css';
 
+// ProgressBar implemented with css transition, and functionality to trigger progress start
+class ProgressBar1 extends Component {
+  constructor(props) {
+    super(props);
+    this.progressBarRef = createRef();
+  }
+
+  componentDidMount() {
+    if (this.props.startImmediately) {
+      this.runAnimation();
+    }
+  }
+
+  runAnimation = () => {
+    const seconds = this.props.totalMs / 1000;
+    this.progressBarRef.current.style.transition = `width ${seconds}s linear`;
+    // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    requestAnimationFrame(() => {
+      this.progressBarRef.current.style.width = '100%';  
+    });
+
+    setTimeout(() => {
+      this.props.onEnd(this.props.id);
+    }, this.props.totalMs);
+  }
+
+  start = () => {
+    this.runAnimation();
+  }
+
+  render() {
+    return (
+      <div className={`${styles['progress-container']} ${this.props.className}`}>
+        <div className={styles['progress-bar']} ref={this.progressBarRef} />
+        <div className={styles['progress-text']}>{`${this.props.totalMs / 1000} s`}</div>
+      </div>
+    );
+  }
+}
+
+ProgressBar1.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.number,
+  onEnd: PropTypes.func,
+  startImmediately: PropTypes.bool,
+  totalMs: PropTypes.number,
+};
+
+ProgressBar1.defaultProps = {
+  className: '',
+  id: null,
+  onEnd: () => {},
+  startImmediately: false,
+  totalMs: 5000,
+};
+
+// ProgressBar implemented with setInterval (not used)
 function ProgressBar2({ className, totalMs }) {
-  const progressBar = useRef(null);
+  const progressBarRef = useRef(null);
 
   useEffect(() => {
-    const seconds = totalMs / 1000;
-    progressBar.current.style.transition = `width ${seconds}s linear`;
-    progressBar.current.style.width = '100%';
+    const intervalMs = totalMs / 100;
+    let width = 0;
+
+    let interval = setInterval(() => {
+      width += 1;
+      if (width > 100) {
+        clearInterval(interval);
+        return;
+      }
+      progressBarRef.current.style.width = `${width}%`;
+    }, intervalMs);
   }, []);
 
   return (
     <div className={`${styles['progress-container']} ${className}`}>
-      <div className={styles['progress-bar']} ref={progressBar} />
+      <div className={styles['progress-bar']} ref={progressBarRef} />
     </div>
   );
 }
@@ -29,38 +94,4 @@ ProgressBar2.defaultProps = {
   totalMs: 5000,
 };
 
-function ProgressBar1({ className, totalMs }) {
-  const progressBar = useRef(null);
-
-  useEffect(() => {
-    const intervalMs = totalMs / 100;
-    let width = 0;
-
-    let interval = setInterval(() => {
-      width += 1;
-      if (width > 100) {
-        clearInterval(interval);
-        return;
-      }
-      progressBar.current.style.width = `${width}%`;
-    }, intervalMs);
-  }, []);
-
-  return (
-    <div className={`${styles['progress-container']} ${className}`}>
-      <div className={styles['progress-bar']} ref={progressBar} />
-    </div>
-  );
-}
-
-ProgressBar1.propTypes = {
-  className: PropTypes.string,
-  totalMs: PropTypes.number,
-};
-
-ProgressBar1.defaultProps = {
-  className: '',
-  totalMs: 5000,
-};
-
-export default ProgressBar2;
+export default ProgressBar1;
--- src/ProgressBars/ProgressBars.js
+++ src/ProgressBars/ProgressBars.js
@@ -1,26 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
+import styles from './ProgressBars.module.css';
 import ProgressBar from './ProgressBar.js';
 
 class Bar {
-  constructor({ id }) {
+  constructor({ id, duration = 3, isComplete = false }) {
     this.id = id;
+    this.duration = duration; // In seconds
+    this.isComplete = isComplete;
   }
 }
 
 function ProgressBars() {
   const [bars, setBars] = useState([]);
+  const barsRef = useRef([]);
+  const secondsInputRef = useRef(null);
+
+  useEffect(() => {
+    const nextBar = bars.find((bar) => !bar.isComplete);
+    if (nextBar) {
+      const barComponent = barsRef.current[nextBar.id]; // The bar's id matches its position
+      barComponent.start();
+    }
+  }, [bars]);
 
   const addBar = () => {
-    const bar = new Bar({ id: bars.length });
+    const seconds = secondsInputRef.current.value !== '' ? Number(secondsInputRef.current.value) : 3; // Default to 3
+    const bar = new Bar({ id: bars.length, duration: seconds });
     setBars((oldBars) => [...oldBars, bar]);
+  };
+
+  const onProgressEnd = (id) => {
+    const updatedBars = bars.map((bar) => {
+      if (bar.id === id) {
+        bar.isComplete = true;
+      }
+      return bar;
+    });
+    setBars(updatedBars);
   };
 
   return (
     <div>
-      <button className="mb-2" onClick={addBar}>Add Bar</button>
-      {bars.map((bar) => (
-        <ProgressBar className="mb-2" key={bar.id} totalMs={3000} />  
+      <label for="seconds">Duration (seconds)</label>
+      <br />
+      <input id={styles['seconds-input']} ref={secondsInputRef} type="number" />
+      <br />
+      <button className="my-2" onClick={addBar}>Add Bar</button>
+      {bars.map((bar, i) => (
+        <ProgressBar
+          className="mb-2"
+          id={bar.id}
+          key={bar.id}
+          onEnd={onProgressEnd}
+          ref={(el) => (barsRef.current[i] = el)} // https://stackoverflow.com/a/57810772
+          totalMs={bar.duration * 1000}
+        />  
       ))}
     </div>
   );
